5075a220200f16d0a15552f5fb3a31d869d144c8
brunocs90 <brunocs90@gmail.com>
2024-12-31 07:42:15 -0300

    Adicionando ao carrinho

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
-import { Search, ShoppingBag } from 'lucide-react';
+import { CartWidget } from '@/components/cart-widget';
+import { Search } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -21,10 +22,7 @@ export function Header() {
       </div>
 
       <div className='flex items-center gap-4'>
-        <div className='flex items-center gap-2'>
-          <ShoppingBag className='h-4 w-4' />
-          <span className='text-sm'>Cart (0)</span>
-        </div>
+        <CartWidget />
 
         <div className='w-px h-4 bg-zinc-700'></div>
 
