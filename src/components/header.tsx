c795fac28fd1fd26f6af7247b5c42fc38996722e
brunocs90 <brunocs90@gmail.com>
2024-12-31 10:22:58 -0300

    Ajustes no deploy da aplicação

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { Suspense } from 'react';
 
 import { CartWidget } from './cart-widget';
 import { SearchForm } from './search-form';
@@ -12,7 +13,9 @@ export function Header() {
           devstore
         </Link>
 
-        <SearchForm />
+        <Suspense fallback={null}>
+          <SearchForm />
+        </Suspense>
       </div>
 
       <div className='flex items-center gap-4'>
@@ -22,13 +25,7 @@ export function Header() {
 
         <Link href='/' className='flex items-center gap-2 hover:underline'>
           <span className='text-sm'>Account</span>
-          <Image
-            src='https://github.com/brunocs90.png'
-            className='h-6 w-6 rounded-full'
-            width={24}
-            height={24}
-            alt=''
-          />
+          <Image src='https://github.com/diego3g.png' className='h-6 w-6 rounded-full' width={24} height={24} alt='' />
         </Link>
       </div>
     </div>
