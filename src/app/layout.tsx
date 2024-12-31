8a62e794a367c22ce78e01c3736f0afdaaa90c0c
brunocs90 <brunocs90@gmail.com>
2024-12-30 21:53:39 -0300

    Page Metadata no Next.js (SEO)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,21 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
 
 export const metadata: Metadata = {
-  title: 'devstore',
+  title: {
+    template: '%s | devstore',
+    default: 'devstore',
+  },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='pt' className={inter.variable}>
+    <html className={inter.variable} lang='pt'>
       <body className='bg-zinc-950 text-zinc-50 antialiased'>{children}</body>
     </html>
   );
